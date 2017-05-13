4c92afa573f986693d0e0acc4cf50b621a377a7c
nestattacked <yang283349242@gmail.com>
2017-05-13 18:36:51 +0800

    modify lightgallery plugin to let it work better

--- source/libs/lightgallery/js/lg-hash.js
+++ source/libs/lightgallery/js/lg-hash.js
@@ -37,11 +37,11 @@
         hash: true
     };
     var Hash = function Hash(element) {
+        this.firstTimeOpen = true;
         this.el = element;
         this.core = window.lgData[this.el.getAttribute('lg-uid')];
         this.core.s = _extends({}, hashDefaults, this.core.s);
         if (this.core.s.hash) {
-            this.oldHash = window.location.hash;
             this.init();
         }
 
@@ -50,19 +50,20 @@
 
     Hash.prototype.init = function () {
         var _this = this;
-        var _hash;
 
-        // Change hash value on after each slide transition
         utils.on(_this.core.el, 'onAfterSlide.lgtm', function (event) {
-            window.location.hash = 'lg=' + _this.core.s.galleryId + '&slide=' + event.detail.index;
+            if(_this.firstTimeOpen){
+                _this.firstTimeOpen = false;
+                window.location.hash = '#lg=' + _this.core.s.galleryId + '&slide=' + event.detail.index;
+            }else{
+                window.location.replace('#lg=' + _this.core.s.galleryId + '&slide=' + event.detail.index);
+            }
         });
 
-        // Listen hash change and change the slide according to slide value
-        utils.on(window, 'hashchange.lghash', function () {
-            _hash = window.location.hash;
+        utils.on(window, 'hashchange.lghash', function (event) {
+            var _hash = window.location.hash;
             var _idx = parseInt(_hash.split('&slide=')[1], 10);
 
-            // it galleryId doesn't exist in the url close the gallery
             if (_hash.indexOf('lg=' + _this.core.s.galleryId) > -1) {
                 _this.core.slide(_idx, false, false);
             } else if (_this.core.lGalleryOn) {
@@ -72,22 +73,12 @@
     };
 
     Hash.prototype.destroy = function () {
-        if (!this.core.s.hash) {
+        if(!this.core.s.hash)
             return;
-        }
-
-        // Reset to old hash value
-        if (this.oldHash && this.oldHash.indexOf('lg=' + this.core.s.galleryId) < 0) {
-            window.location.hash = this.oldHash;
-        } else {
-            if (history.pushState) {
-                history.pushState('', document.title, window.location.pathname + window.location.search);
-            } else {
-                window.location.hash = '';
-            }
-        }
 
         utils.off(this.core.el, '.lghash');
+        if (window.location.hash.indexOf('lg=' + this.core.s.galleryId) > -1)
+            window.history.back();
     };
 
     window.lgModules.hash = Hash;
